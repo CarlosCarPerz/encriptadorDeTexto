6be238ed17a7df0ebb84d34813b083b7af48984d
Carlos Carvajal <97265469+CarlosCarPerz@users.noreply.github.com>
2023-03-29 14:42:17 -0600

    aun no se puede ver el mensaje

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const textArea = document.querySelector(".ingreso");
-const mesage = document.querySelector(".mensaje");
+const textIngreso = document.querySelector(".ingreso");
+const mesage = document.querySelector("#textFinal");
+
+
+/* const btnEncriptar = document.querySelector("#encriptarBtn"); */
 
 /* La letra "e" es convertida para "enter"
 La letra "i" es convertida para "imes"
@@ -7,4 +10,25 @@ La letra "a" es convertida para "ai"
 La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat" */
 
-let matrizCod = [["e", "enter"], ["i", "imes"],["a","ai"], ["o", "ober"], ["u","ufat"]];
+/* function clickBtnEncriptar () {
+    btnEncriptar.addEventListener("click", encriptarBtn);
+}
+ */
+function encriptarBtn () {
+    const textoEncriptado = encriptar(textIngreso.value);
+    mesage.style.display = inline-block;
+    mesage.value = textoEncriptado;
+    textIngreso.value = ""
+}
+
+function encriptar (stringEncriptado) {
+    let matrizCod = [["e", "enter"], ["i", "imes"],["a","ai"], ["o", "ober"], ["u","ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase();
+
+    for(let i = 0; i < matrizCod.length; i++) {
+        if (stringEncriptado.includes(matrizCod[i][0])) {
+            stringEncriptado = stringEncriptado.replaceAll(matrizCod[i][0],matrizCod [i][1])
+        }
+    }
+    return stringEncriptado
+}
