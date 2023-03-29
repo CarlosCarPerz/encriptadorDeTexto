7fe60342db2372b0c9cc5c7ab3b3cc6eb970c439
Carlos Carvajal <97265469+CarlosCarPerz@users.noreply.github.com>
2023-03-28 20:00:32 -0600

    se crea script y se inicia logica, se modifica css

---
+++ script.js
@@ -0,0 +1,10 @@
+const textArea = document.querySelector(".ingreso");
+const mesage = document.querySelector(".mensaje");
+
+/* La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" */
+
+let matrizCod = [["e", "enter"], ["i", "imes"],["a","ai"], ["o", "ober"], ["u","ufat"]];
